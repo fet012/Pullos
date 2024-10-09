ed3705fb6f3b2106219db0746c1da71d64317082
FalolaTemidayo <falolatemidayoemm@gmail.com>
2024-10-09 03:37:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const scrollSections = document.querySelectorAll(".scroll-section");
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  scrollSections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    if (
+      scrollPosition >= sectionTop - 600 &&
+      scrollPosition < sectionTop + sectionHeight
+    ) {
+      section.classList.add("visible");
+    } else {
+      section.classList.remove("visible");
+    }
+  });
+});
+
+
